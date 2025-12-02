dc4f02254e0532121f119b7d5a3fc8edc3b648e4
Pattou Emad <pattouemad@Partinias-MacBook.local>
2025-12-02 18:28:38 +0200

    messages page

--- src/Pages/Messages.jsx
+++ src/Pages/Messages.jsx
@@ -3,8 +3,11 @@ import '../Pages/Messages.css' ;
 import Aside from '../Components/Aside';
 import Nav from '../Components/Nav';
 import Title from '../Components/Title' ;
+import Topbox from '../Components/Topbox';
+import myimg from '../Images/myimg.png'
+import trash from '../Images/delete.svg'
 import Pagenumber from '../Components/Pagenumber';
-
+import Footer from '../Components/Footer';
 const Messages = () => {
     return ( <>
     
@@ -19,19 +22,125 @@ const Messages = () => {
 
 <div className='top-messages'>
 <Title title="Messages" />
-<Pagenumber />
+
+<div className='right-msg'>
+ 
+<button className='allmsg'>All</button>
+<button className='allmsgs'>Unread</button>
+<button className='allmsgs'>Important</button>
+
+</div>
+
+</div>
+
+<div className='topboxes'>
+<Topbox 
+  title1="Overall mesages"
+  title2="{2389}"
+  title3="This month"
+/>
+
+<Topbox 
+  title1="Sent messages"
+  title2="{1494}"
+  title3="This month"
+/>
+
+<Topbox 
+  title1="Received messages"
+  title2="{895}"
+  title3="This month"
+/>
+</div>
+
+<div className='allmsgsfield'>
+
+<div className='msgs-type'>
+<div className='tick'></div>
+<img className='sender-img' src={myimg} alt="sender image" />
+<h4 className='sender-name'>Partinia Boktor</h4>
+<button className='user'>User testing</button>
+<p className='sender-msg'>Hi partinia , We need to do user testing by Monday ,<br></br> Please add  me in project group </p>
+<img src={trash} alt="" />
 </div>
 
 
 
 
+<div className='msgs-type'>
+<div className='tick'></div>
+<img className='sender-img' src={myimg} alt="sender image" />
+<h4 className='sender-name'>Partinia Boktor</h4>
+<button className='user'>User testing</button>
+<p className='sender-msg'>Hi partinia , We need to do user testing by Monday ,<br></br> Please add  me in project group </p>
+<img src={trash} alt="" />
+</div>
+
+
+
+<div className='msgs-type'>
+<div className='tick'></div>
+<img className='sender-img' src={myimg} alt="sender image" />
+<h4 className='sender-name'>Partinia Boktor</h4>
+<button className='user'>User testing</button>
+<p className='sender-msg'>Hi partinia , We need to do user testing by Monday ,<br></br> Please add  me in project group </p>
+<img src={trash} alt="" />
+</div>
+
+
+
+<div className='msgs-type'>
+<div className='tick'></div>
+<img className='sender-img' src={myimg} alt="sender image" />
+<h4 className='sender-name'>Partinia Boktor</h4>
+<button className='user'>User testing</button>
+<p className='sender-msg'>Hi partinia , We need to do user testing by Monday ,<br></br> Please add  me in project group </p>
+<img src={trash} alt="" />
+</div>
 
 
 
 
+<div className='msgs-type'>
+<div className='tick'></div>
+<img className='sender-img' src={myimg} alt="sender image" />
+<h4 className='sender-name'>Partinia Boktor</h4>
+<button className='user'>User testing</button>
+<p className='sender-msg'>Hi partinia , We need to do user testing by Monday ,<br></br> Please add  me in project group </p>
+<img src={trash} alt="" />
+</div>
 
 
 
+
+<div className='msgs-type'>
+<div className='tick'></div>
+<img className='sender-img' src={myimg} alt="sender image" />
+<h4 className='sender-name'>Partinia Boktor</h4>
+<button className='user'>User testing</button>
+<p className='sender-msg'>Hi partinia , We need to do user testing by Monday ,<br></br> Please add  me in project group </p>
+<img src={trash} alt="" />
+</div>
+
+
+
+<div className='msgs-type'>
+<div className='tick'></div>
+<img className='sender-img' src={myimg} alt="sender image" />
+<h4 className='sender-name'>Partinia Boktor</h4>
+<button className='user'>User testing</button>
+<p className='sender-msg'>Hi partinia , We need to do user testing by Monday ,<br></br> Please add  me in project group </p>
+<img src={trash} alt="" />
+</div>
+
+<div className='end'>
+    <button className='previous'>Previous</button>
+    <Pagenumber title="Page 1 of 10" />
+     <button className='next'>Next</button>
+</div>
+
+</div>
+<Footer />
 </div>
     </div>
     </> );
